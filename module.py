fa881051823149903608a2b9732634855baf404c
githead927 <githubb2024@gmail.com>
2024-10-29 10:41:56 +0530

    Basic01

--- module.py
+++ module.py
@@ -1,7 +1,8 @@
 import pyjokes
 
 # single line comment print("printing...")
-# this is a multi line comment
+#this is a multi line comment
 """i am shark"""
+#for multi line comment select the whole para or sentence and press command+/ forward slash
 joke = pyjokes.get_joke()
 print(joke)
