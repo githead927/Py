75313683ebc7687cf29cb7f470ac3d4492ea625a
githead927 <githubb2024@gmail.com>
2024-11-04 11:07:13 +0530

    package changes

---
+++ chapter2/04_operators.py
@@ -0,0 +1,11 @@
+#Following are some common operators in python:
+'''
+1. Arithmetic operators: +, -, *, / etc.
+
+2. Assignment operators: =, +=, -= etc.
+
+3. Comparison operators: ==, >, >=, <, != etc.
+
+4. Logical operators: and, or, not.
+
+'''
